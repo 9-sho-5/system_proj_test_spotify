3c0dcb06b3872999e28529e8b5b6c198162e725e
9-sho-5 <hosho.kusu@icloud.com>
2022-11-03 14:46:12 +0900

    fix: redirect method

--- WebContent/api/assets/javascript/index.js
+++ WebContent/api/assets/javascript/index.js
@@ -1,21 +1,42 @@
 var xmlHttpRequest;
+let res_uri;
 
-function sendWithPostMethod() {	
+function sendWithGetMethod() {	
 	var url = "login";
 	xmlHttpRequest = new XMLHttpRequest();
+	xmlHttpRequest.withCredentials = true;
 	xmlHttpRequest.onreadystatechange = receive;
-	xmlHttpRequest.open("post", url, true);
+	xmlHttpRequest.open("get", url, true);
 	xmlHttpRequest.send(null);
+}
+
+function redirect_to_api_auth() {
+
+	window.location.href = res_uri;
+
+	// const xhr = new XMLHttpRequest();
+	// xhr.open("get", res_uri);
+	// xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+	// xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+	// xhr.setRequestHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token, X-HTTP-Method-Override, X-Requested-With, api-token');
+	// xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
+	// xhr.withCredentials = true;
+	// xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencode');
+	// xhr.send();
+
+	// console.log(xhr.getResponseHeader("Access-Control-Allow-Methods"))
 }
 
 function receive() {
 	if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
 		var response = JSON.parse(xmlHttpRequest.responseText);
-		console.log(response);
+		res_uri = response.uri;
+		console.log(res_uri)
+		redirect_to_api_auth();
 	}
 }
 
 window.addEventListener("load", function() {
     var getButtonElement = document.querySelector("button");
-    getButtonElement.addEventListener("click", sendWithPostMethod, false);   
+    getButtonElement.addEventListener("click", sendWithGetMethod, false);   
 });
--- src/api/LoginServlet.java
+++ src/api/LoginServlet.java
@@ -1,23 +1,28 @@
 package api;
 
 import java.io.Writer;
+import java.util.Enumeration;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 /**
  * Spotify Login Servlet
  */
 @WebServlet("/api/login")
-public class LoginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet implements Filter {
 
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request,
+	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
         
 		String auhorizeUri = null;
@@ -25,7 +30,17 @@ public class LoginServlet extends HttpServlet {
 		Spotify spotify = new Spotify();
 		auhorizeUri = spotify.authorize();
 
-		response.setContentType("application/json");
+		response.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token, X-HTTP-Method-Override, X-Requested-With, api-token");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+
+		// System.out.println(response.containsHeader("Access-Control-Allow-Origin"));
+		// 		doOptions(request, response);
+		// response.sendRedirect(auhorizeUri);
+		// response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+		// response.setHeader("Location", auhorizeUri);
+		// response.setContentType("application/json");
 		StringBuilder builder = new StringBuilder();
 		builder.append('{');
 		builder.append("\"uri\":\"").append(auhorizeUri).append("\"");
@@ -36,6 +51,19 @@ public class LoginServlet extends HttpServlet {
 		Writer writer = response.getWriter();
 		writer.append(json);
 		writer.flush();
-
+		
     }
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 }
