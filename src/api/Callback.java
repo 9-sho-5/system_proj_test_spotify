683657447bfaab216747a56427fee770371dcd78
9-sho-5 <hosho.kusu@icloud.com>
2022-12-01 23:42:56 +0900

    fix: create Callback.java & if params has code, setCode to Spotify.java then set login status on sesion

---
+++ src/api/Callback.java
@@ -0,0 +1,35 @@
+package api;
+
+import java.io.Writer;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/api/callback")
+public class Callback extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();	
+        
+        String code = (request.getParameter("code") == null) ? "" : (String) request.getParameter("code");
+        if (code != null){
+            Spotify spotify = new Spotify();
+            spotify.setCode(code);
+            session.setAttribute("loginStatus", "loggedIn");
+        } else {
+            session.setAttribute("loginStatus", "notloggedIn");
+        }
+
+        request.setCharacterEncoding("UTF-8");
+		response.sendRedirect("index.html");
+		
+	}
+    
+}
