b7655187ab94d364175589a397b59ff594a7a12c
9-sho-5 <hosho.kusu@icloud.com>
2022-12-02 00:22:22 +0900

    fix: use cookie to set login status

--- src/api/Callback.java
+++ src/api/Callback.java
@@ -4,6 +4,7 @@ import java.io.Writer;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,16 +18,18 @@ public class Callback extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         HttpSession session = request.getSession();	
+        Cookie cookie = null;
         
         String code = (request.getParameter("code") == null) ? "" : (String) request.getParameter("code");
         if (code != null){
             Spotify spotify = new Spotify();
             spotify.setCode(code);
-            session.setAttribute("loginStatus", "loggedIn");
+            cookie = new Cookie("loginStatus", "loggedIn");
         } else {
-            session.setAttribute("loginStatus", "notloggedIn");
+            cookie = new Cookie("loginStatus", "notLoggedIn");
         }
-
+        
+        response.addCookie(cookie);
         request.setCharacterEncoding("UTF-8");
 		response.sendRedirect("index.html");
 		
